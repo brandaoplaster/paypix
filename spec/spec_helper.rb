b741a39944441d5fa522b12553ec154959c5a306
brandaoplaster <brandaoplaster@gmail.com>
2023-02-11 12:01:52 -0300

    chore: Adding rspec setting disable connection

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,15 @@
 # frozen_string_literal: true
 
 require "paypix"
+require "webmock/rspec"
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
+
+  config.before(:each) do
+    WebMock.disable_net_connect!(allow_localhost: true)
+  end
 
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
