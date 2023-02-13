47172ee5e0a731f0f377e0197b639b173d783fae
brandaoplaster <brandaoplaster@gmail.com>
2023-02-12 22:31:01 -0300

    feat: Adding paypix and test

--- lib/paypix.rb
+++ lib/paypix.rb
@@ -1,8 +1,19 @@
 # frozen_string_literal: true
 
 require_relative "paypix/version"
+require_relative "paypix/client/api_client"
+require_relative "paypix/helpers/helpers"
 
 module Paypix
-  class Error < StandardError; end
-  # Your code goes here...
+  class Pix
+    def initialize
+      @api_client = ApiClient.new
+    end
+
+    def create(attributes)
+      if Helpers.valid_hash?(attributes)
+        @api_client.post("charge/pix", attributes)
+      end
+    end
+  end
 end
--- spec/paypix_spec.rb
+++ spec/paypix_spec.rb
@@ -1,11 +1,68 @@
 # frozen_string_literal: true
 
 RSpec.describe Paypix do
-  it "has a version number" do
-    expect(Paypix::VERSION).not_to be nil
+  context "Creating Pix" do
+    let(:path) { "charge/pix" }
+    let(:url) { "#{ApiClient::BASE_URL}#{path}" }
+    let(:request_headers) do
+      {
+        "Accept" => "*/*",
+        "Accept-Encoding" => "gzip;q=1.0,deflate;q=0.6,identity;q=0.3",
+        "Content-Type" => "application/json",
+        "Host" => "api-sandbox.kobana.com.br",
+        "User-Agent" => "Ruby",
+      }
+    end
+
+    context "successful creation" do
+      let(:response_body) {
+        {
+          :status => 201,
+          :data => {
+            :id => 42,
+            :status => "opened",
+            :pix_account_id => 42,
+            :created_at => "2023-02-09T08:16:18+12:00",
+            :expire_at => "2023-02-19T08:16:18+12:00",
+            :amount => 100.5,
+            :payer => {
+              :document_number => "652.388.162-86",
+              :name => "John Doe",
+            },
+          },
+        }.to_json
+      }
+      let(:response) { double("response", body: response_body) }
+      let(:data) {
+        {
+          "payer": {
+            "document_number": "652.388.162-86",
+            "name": "John Doe",
+          },
+          "amount": 100.5,
+          "pix_account_id": 1,
+          "expire_at": "2023-02-19T08:16:18+12:00",
+          "message": "Hello word",
+        }
+      }
+      before do
+        stub_request(:post, url)
+          .with(body: data.to_json, headers: request_headers)
+          .to_return(status: 201, body: response_body)
+      end
+
+      it "when parameters are valid" do
+        pix = Paypix::Pix.new
+
+        respone = pix.create(data)
+
+        expect(respone).to include("status" => 201)
+        expect(WebMock).to have_requested(:post, url).with(body: data.to_json, headers: request_headers)
+      end
+    end
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "has a version number" do
+    expect(Paypix::VERSION).not_to be nil
   end
 end
