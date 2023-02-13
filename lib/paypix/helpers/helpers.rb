fa6406ec3798ce3d9702dfaa6c925a3144ab4a8f
brandaoplaster <brandaoplaster@gmail.com>
2023-02-12 22:29:59 -0300

    refactor: Refactoring module helpers and tests

--- lib/paypix/helpers/helpers.rb
+++ lib/paypix/helpers/helpers.rb
@@ -1,11 +1,17 @@
 # frozen_string_literal: true
 
 module Helpers
-  def self.get_key_in_hash(attributes, key)
-    if attributes.has_key?(key)
-      attributes[key]
-    else
-      nil
+  def self.valid_hash?(hash)
+    required_keys = [:amount, :pix_account_id, :expire_at]
+    expected_types = { amount: Float, payer: Hash, pix_account_id: Integer, txid: String }
+
+    required_keys.each do |key|
+      raise ArgumentError, "Key #{key} is required and not found in the hash" unless hash.key?(key)
     end
+
+    true
+  rescue ArgumentError => e
+    puts e.message
+    false
   end
 end
--- spec/helpers/helpers_spec.rb
+++ spec/helpers/helpers_spec.rb
@@ -2,28 +2,41 @@ require "spec_helper"
 require "paypix/helpers/helpers"
 
 RSpec.describe Helpers do
-  context "recovering values with key exists" do
-    let(:data) {
-      {
-        :payer => {
-          :document_number => "111.222.333-04",
-          :name => "Mike",
-        },
-        :charge => {
+  context "validating the input hash" do
+    context "valid hash" do
+      let(:data) {
+        {
+          :payer => {
+            :document_number => "111.222.333-04",
+            :name => "Mike",
+          },
           :amount => 10.50,
           :pix_account_id => 90,
           :expire_at => "2023-12-02 10:03:56",
           :message => "Hello word",
-        },
+        }
       }
-    }
 
-    it "when the hash has the key then it returns value" do
-      expect(Helpers.get_key_in_hash(data, :charge)).to be(data[:charge])
+      it "when the hash is valid it returns true" do
+        expect(Helpers.valid_hash?(data)).to be(true)
+      end
     end
 
-    it "when the hash does not have the key then it returns nil" do
-      expect(Helpers.get_key_in_hash(data, :additional_info)).to be_nil
+    context "invalid hash" do
+      let(:data) {
+        {
+          :payer => {
+            :document_number => "111.222.333-04",
+            :name => "Mike",
+          },
+          :amount => 10.50,
+          :pix_account_id => 90,
+          :message => "Hello word",
+        }
+      }
+      it "when the hash is not valid it returns false" do
+        expect(Helpers.valid_hash?(data)).to be(false)
+      end
     end
   end
 end
